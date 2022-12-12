169c3690be016d45a0ac7b304d29e73c7071099a
martintodorov4040 <119971043+martintodorov4040@users.noreply.github.com>
2022-12-12 17:52:52 +0100

    finish homework

---
+++ JS- zodiac/script.js
@@ -0,0 +1,44 @@
+
+let = (2003 -4)%12;
+
+switch ((2003 -4)%12) {
+  case 1:
+    console.log("You are a Rat");
+    break;
+  case 2:
+    console.log("You are Ox");
+    break;
+  case 3:
+    console.log("You are Tiger");
+    break;
+  case 4:
+    console.log("You are Rabbit");
+    break;
+  case 5:
+    console.log("You are Dragon");
+    break;
+    case 6:
+    console.log("You are Snake");
+    break;
+    case 7:
+    console.log("You are Horse");
+    break;
+    case 8:
+    console.log("You are Goat");
+    break;
+    case 9:
+    console.log("You are Monkey");
+    break;
+    case 10:
+    console.log("You are Rooster");
+    break;
+    case 11:
+    console.log("You are Dog");
+    break;
+    case 12:
+    console.log("You are Pig");
+     break;
+           
+  default:
+    console.log("Sorry");
+}
---
+++ JS-Calculator for dog year/script.js
@@ -0,0 +1,6 @@
+let year = 5;
+function calculateDog(){
+let dogYear = (year *7);
+console.log(`Your dog has: ${dogYear} years.`);
+}
+calculateDog();
